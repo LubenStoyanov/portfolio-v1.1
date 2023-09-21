0f098110e1241d0bfbbe7a2fd405c4f1c6a3b836
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-09-21 14:49:45 +0200

    Add typewriter effect

--- src/main.js
+++ src/main.js
@@ -2,3 +2,27 @@ const copyright = document.querySelector(".copyrigth");
 const date = new Date();
 copyright.textContent += `${date.getFullYear()} Luben Stoyanov. All rights reserved.`;
 
+const app = document.querySelector("#typewriter");
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .typeString('I create the web.')
+  .pauseFor(300)
+  .deleteChars(17)
+  .typeString('I ride a bicycle.')
+  .pauseFor(300)
+  .deleteChars(17)
+  .typeString('I play chess.')
+  .pauseFor(300)
+  .deleteChars(13)
+  .typeString('I lift weights.')
+  .pauseFor(300)
+  .deleteChars(15)
+  .typeString('I play the piano.')
+  .pauseFor(300)
+  .deleteChars(17)
+  .start();
