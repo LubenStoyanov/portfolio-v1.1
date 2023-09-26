fcb79db7fae34acce6084c81be3f6e8ee20bf3cd
Luben Stoyanov <luben.stoyanov.ls@gmail.com>
2023-09-26 15:13:54 +0200

    Remove lazy loading img, update header about page

--- src/main.js
+++ src/main.js
@@ -4,26 +4,28 @@ copyright.textContent += `${date.getFullYear()} Luben Stoyanov. All rights reser
 
 const app = document.querySelector("#typewriter");
 
-var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: "natural",
-  deleteSpeed: "natural",
-});
+if (!window.location.href.endsWith("about/")) {
+  let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: "natural",
+    deleteSpeed: "natural",
+  });
 
-window.onload = () => typewriter
-  .typeString('I create the web.')
-  .pauseFor(300)
-  .deleteChars(17)
-  .typeString('I ride a bicycle.')
-  .pauseFor(300)
-  .deleteChars(17)
-  .typeString('I play chess.')
-  .pauseFor(300)
-  .deleteChars(13)
-  .typeString('I lift weights.')
-  .pauseFor(300)
-  .deleteChars(15)
-  .typeString('I play the piano.')
-  .pauseFor(300)
-  .deleteChars(17)
-  .start();
+  typewriter
+    .typeString('I create the web.')
+    .pauseFor(300)
+    .deleteChars(17)
+    .typeString('I ride a bicycle.')
+    .pauseFor(300)
+    .deleteChars(17)
+    .typeString('I play chess.')
+    .pauseFor(300)
+    .deleteChars(13)
+    .typeString('I lift weights.')
+    .pauseFor(300)
+    .deleteChars(15)
+    .typeString('I play the piano.')
+    .pauseFor(300)
+    .deleteChars(17)
+    .start();
+}
